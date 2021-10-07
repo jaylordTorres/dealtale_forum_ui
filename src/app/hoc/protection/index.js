270f0bbd74c92e65fd7929b128865468e91be199
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-10-07 12:10:13 +0800

    protect using session id

--- src/app/hoc/protection/index.js
+++ src/app/hoc/protection/index.js
@@ -1,9 +1,12 @@
 import { AccessDenied } from "../../component";
+import { useSession } from "../../provider/session/hook";
 
 export const withProtection =
   (Component, FallBack = AccessDenied) =>
   (props) => {
-    console.log("protected");
+    const { id } = useSession();
+    if (id) {
+      return <Component {...props} />;
+    }
     return <FallBack />;
-    return <Component {...props} />;
   };
