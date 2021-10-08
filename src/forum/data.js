a3e65bc5082c13787d292a3fa2af2068d58e7456
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-10-08 18:21:27 +0800

    fix typo

--- src/forum/data.js
+++ src/forum/data.js
@@ -1,16 +1,19 @@
 export const forumTestData = [
   {
     id: "1",
+    sessionId: "93248099649673",
     title: "sample title 1",
     content: "sample content 1",
   },
   {
     id: "2",
+    sessionId: "93248099649672",
     title: "sample title 2",
     content: "sample content 2",
   },
   {
     id: "3",
+    sessionId: "93248099649674",
     title: "sample title 3",
     content: "sample content 3",
   },
