186d8f69d4969c760499055891e92a843c257cd2
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-10-08 19:13:58 +0800

    rest event

--- src/app/util/request.js
+++ src/app/util/request.js
@@ -3,14 +3,32 @@ import { config } from "../constant";
 import { store } from "../service";
 
 export async function fetcher({ queryKey }) {
+  const init = await getInit();
+
+  return (await axios.get(`${config.api}/${queryKey[0]}`, init)).data;
+}
+
+export async function poster(url, data) {
+  const init = await getInit();
+
+  return (await axios.post(`${config.api}/${url}`, data, init)).data;
+}
+
+export async function patcher(url, data) {
+  const init = await getInit();
+
+  return (await axios.patch(`${config.api}/${url}`, data, init)).data;
+}
+
+async function getInit(headers = {}) {
   const session = await store.session.getItem();
-  const init = {
+
+  return {
     headers: {
       "content-type": "application/json",
       app_client_key: config.client_key,
       app_session_id: session ? session.id : null,
+      ...headers,
     },
   };
-
-  return (await axios.get(`${config.api}/${queryKey[0]}`, init)).data;
 }
