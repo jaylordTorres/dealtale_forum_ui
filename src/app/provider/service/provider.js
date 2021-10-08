ddc0a59334ee024c6179b6f48d7e5bb2f944ee64
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-10-08 18:19:13 +0800

    use react-query

--- src/app/provider/service/provider.js
+++ src/app/provider/service/provider.js
@@ -1,10 +1,15 @@
 import { ServiceContext } from "./constant";
 import { useServiceProvider } from "./hook";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient();
 
 export function ServiceProvider({ store, children }) {
   const value = useServiceProvider({ store });
 
   return (
-    <ServiceContext.Provider value={value}>{children}</ServiceContext.Provider>
+    <ServiceContext.Provider value={value}>
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+    </ServiceContext.Provider>
   );
 }
