ff4cf448943c75b5e736134941ae5c24ac20641a
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-10-08 20:22:22 +0800

    forum form ui

--- src/forum/component/forum_form/index.js
+++ src/forum/component/forum_form/index.js
@@ -1,36 +1,40 @@
+import { Button } from "../../../app/component";
 import { useForumForm } from "./hook";
+import { StyledForumForm } from "./styled";
 
 export function ForumForm({ initialValues, onSubmit, onCancel }) {
   const { formik } = useForumForm({ initialValues, onSubmit });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <input
-          type="text"
-          name="title"
-          onChange={formik.handleChange}
-          value={formik.values.title}
-        />
-      </div>
-      <div>
-        <textarea
-          type="text"
-          multiple
-          rows={4}
-          name="content"
-          onChange={formik.handleChange}
-          value={formik.values.content}
-        />
-      </div>
+    <StyledForumForm>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          />
+        </div>
+        <div>
+          <textarea
+            type="text"
+            multiple
+            rows={4}
+            name="content"
+            onChange={formik.handleChange}
+            value={formik.values.content}
+          />
+        </div>
 
-      <div>{formik.errors.form}</div>
-      <div>
-        <button type="button" onClick={onCancel}>
-          cancel
-        </button>
-        <button type="submit">save</button>
-      </div>
-    </form>
+        <div>{formik.errors.form}</div>
+        <div>
+          <Button type="button" onClick={onCancel}>
+            cancel
+          </Button>
+          <Button type="submit">save</Button>
+        </div>
+      </form>
+    </StyledForumForm>
   );
 }
---
+++ src/forum/component/forum_form/styled.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const StyledForumForm = styled("div")`
+  input,
+  textarea {
+    padding: 8px;
+    font-size: 16px;
+    margin: 8px;
+    width: 100%;
+  }
+`;
