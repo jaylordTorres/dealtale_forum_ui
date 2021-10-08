940f7327fef6176a8ef574e63e4e3b4b00efe91e
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-10-08 20:22:05 +0800

    button ui

---
+++ src/app/component/button/styled.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const StyleButton = styled("button")`
+  font-size: 16px;
+  padding: 16px;
+  cursor: pointer;
+  border: 0;
+  border-radius: 0;
+  background: none;
+  background-color: #f67e7d;
+  margin: 8px;
+  text-transform: uppercase;
+  -webkit-appearance: none;
+`;
