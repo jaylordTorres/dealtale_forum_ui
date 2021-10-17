b4f24eb8045e1de42a3ab5326cb8cbad1d5dc314
Jay Lord P. Torres <isens.jaylord@gmail.com>
2021-10-17 19:31:06 +0800

    forum hook test

--- src/forum/provider/hook.test.js
+++ src/forum/provider/hook.test.js
@@ -1,46 +1,72 @@
 import { cleanup } from "@testing-library/react";
-import { renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
+import { QueryClientInstance } from "../../app/provider";
 import { createTestProviers } from "../../app/util/test";
 import { forums } from "../data";
 import { useForum, useForumProvider } from "./hook";
-import { getForumTestUtils, services } from "./test";
+import { api, getForumTestWrapper, services } from "./test";
 
 describe("unit test: forum/provider/hook: useForum", () => {
-  afterAll(cleanup);
-  const { wrapper } = getForumTestUtils();
-  const { result } = renderHook(() => useForum(), { wrapper });
+  const wrapper = getForumTestWrapper();
+  let rendered;
+  beforeEach(async () => {
+    jest.spyOn(api, "fetcher").mockImplementation(() => {
+      return { data: forums };
+    });
+    await act(async () => {
+      rendered = renderHook(() => useForum(), {
+        wrapper,
+      });
+    });
+  });
 
+  afterEach(() => {
+    cleanup();
+    QueryClientInstance.clear();
+  });
   it("should correctly defined exports", async () => {
     expect(useForum).toBeDefined();
   });
 
   it("should give correct return types", async () => {
-    expect(result.current.values).toEqual(expect.any(Array));
-    expect(result.current.update).toEqual(expect.any(Function));
-    expect(result.current.create).toEqual(expect.any(Function));
+    expect(rendered.result.current.values).toEqual(expect.any(Array));
+    expect(rendered.result.current.update).toEqual(expect.any(Function));
+    expect(rendered.result.current.create).toEqual(expect.any(Function));
   });
 
   it("should fetch valid data", async () => {
-    expect(result.current.values).toStrictEqual(forums);
+    expect(rendered.result.current.values).toStrictEqual(forums);
   });
 });
 
 describe("unit test: forum/provider/hook: useForumProvider", () => {
-  afterAll(cleanup);
   const wrapper = createTestProviers(services);
-  const { result } = renderHook(() => useForumProvider(), { wrapper });
+  let rendered;
+  beforeEach(async () => {
+    jest.spyOn(api, "fetcher").mockImplementation(() => {
+      return { data: forums };
+    });
+    await act(async () => {
+      rendered = renderHook(() => useForumProvider(), { wrapper });
+    });
+  });
+
+  afterEach(() => {
+    cleanup();
+    QueryClientInstance.clear();
+  });
 
   it("should correctly defined exports", async () => {
     expect(useForumProvider).toBeDefined();
   });
 
   it("should give correct return types", async () => {
-    expect(result.current.values).toEqual(expect.any(Array));
-    expect(result.current.update).toEqual(expect.any(Function));
-    expect(result.current.create).toEqual(expect.any(Function));
+    expect(rendered.result.current.values).toEqual(expect.any(Array));
+    expect(rendered.result.current.update).toEqual(expect.any(Function));
+    expect(rendered.result.current.create).toEqual(expect.any(Function));
   });
 
   it("should fetch valid data", async () => {
-    expect(result.current.values).toStrictEqual(forums);
+    expect(rendered.result.current.values).toStrictEqual(forums);
   });
 });
